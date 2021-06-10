84449bc21dc437cf9f81ba1ab19a5a48834c4a31
yeezick <ramerick5@gmail.com>
2021-06-10 19:25:24 -0400

    kept working on css styling. + touched up responsiveness. + added backgrounds to forecast + found new news API (MVP IS SATISFIED WHOOP WHOOP)

--- index.js
+++ index.js
@@ -60,9 +60,8 @@ getQuotes(inspoUrl);
   News
 ======================*/
 
-
 const newsUrl =
-"https://newsapi.org/v2/top-headlines?country=us&apiKey=6f7081542a3346a4865b765624dc3a48";
+  "http://api.mediastack.com/v1/news?access_key=6093edc68c7322dfa2913f7433667f4d&countries=us";
 
 const newsDropdown = document.querySelector("#publisher-dropdown");
 const newsText = document.querySelector(".news-text");
@@ -70,38 +69,43 @@ const headline = document.querySelector(".headline");
 const author = document.querySelector(".author");
 
 // callback function make a new option elment for each publisher
-function publisherOptions (articles) {
+function publisherOptions(articles) {
   // set default article
   newsText.innerText = articles[0].description;
   headline.innerText = articles[0].title;
   author.innerText = articles[0].author;
-  
+
   let count = 0;
-  articles.forEach(article => {
+  articles.forEach((article) => {
     // create option element with inner text set to publisher brand
     const option = document.createElement("option");
-    option.innerText = article.source.name;
-    option.setAttribute("value", count )
+    // console.log(article)
+    if (article.author === null) {
+      option.innerText = "Anonymous";
+    } else {
+      option.innerText = article.author;
+    }
+    option.setAttribute("value", count);
     newsDropdown.appendChild(option);
-    count +=1;
+    count += 1;
   });
 }
 // function to change news content based on user choice
 function changeNews(articles) {
   console.log(articles);
   const newPublisher = newsDropdown.value;
-  console.log(newPublisher)
+  console.log(newPublisher);
   newsText.innerText = articles[newPublisher].description;
   headline.innerText = articles[newPublisher].title;
   author.innerText = articles[newPublisher].author;
 }
 
-
 const getNews = async (url) => {
   try {
     const response = await axios.get(url);
-    const articles = response.data.articles;
-    newsDropdown.addEventListener("change", () => changeNews(articles))
+    const articles = response.data.data;
+
+    newsDropdown.addEventListener("change", () => changeNews(articles));
     // console.log(articles[0].source)
     publisherOptions(articles);
     console.log(articles);
@@ -109,9 +113,7 @@ const getNews = async (url) => {
     console.log(error);
   }
 };
-getNews(newsUrl)
-
-
+getNews(newsUrl);
 
 /*=====================
 Weather
@@ -121,7 +123,7 @@ const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=miami&units
 
 const temperature = document.querySelector("#temp");
 const description = document.querySelector("#description");
-const weatherImage = document.querySelector(".weather-image");
+const weatherImage = document.querySelector(".weather-image > img");
 const high = document.querySelector("#high");
 const low = document.querySelector("#low");
 const humidity = document.querySelector("#humidity");
@@ -130,11 +132,11 @@ const humidity = document.querySelector("#humidity");
 function firstLetterCaseWeather(description) {
   let strArr = description.split("");
   let firstLetter = strArr[0];
-  let capLetter = firstLetter.toUpperCase();  
-  strArr.splice(0,1,capLetter)
-  let readyCased = strArr.join('');
+  let capLetter = firstLetter.toUpperCase();
+  strArr.splice(0, 1, capLetter);
+  let readyCased = strArr.join("");
   desc = readyCased;
-  console.log(desc);
+  // console.log(desc);
   return desc;
 }
 
@@ -142,10 +144,10 @@ function firstLetterCaseWeather(description) {
 function weatherChanger(url) {
   // changes the values on html to current data based on api
   const main = url.data.main;
-  const desc = "thunderstorm"
+  const desc = "clear sky";
   // url.data.weather[0].description;
-  console.log(desc);
-  
+  // console.log(desc);
+
   temperature.innerText = `${main.temp}°F`;
   high.innerText = `${main.temp_max}°F`;
   low.innerText = `${main.temp_min}°F`;
@@ -155,46 +157,62 @@ function weatherChanger(url) {
   // depending on case, image attribute will have different icon
   let iconCode = null;
   const forecastContainer = document.querySelector(".forecast");
-  console.log(forecastContainer.style)
 
   switch (desc) {
     case "clear sky":
       iconCode = "01d";
+      forecastContainer.style.background =
+        "url(https://media0.giphy.com/media/VxbvpfaTTo3le/giphy.gif?cid=ecf05e4733z2ls10lumuj6ubt31ta8zpzydt3chqkkg01nsu&rid=giphy.gif&ct=g) no-repeat center center / cover";
       break;
     case "few clouds":
       iconCode = "02d";
+      forecastContainer.style.background =
+        "url(https://media2.giphy.com/media/c31WXGK1jLQBy/giphy.gif?cid=ecf05e471onzfrnepln511m6mm6ly4brsfxn0styxspk17ft&rid=giphy.gif&ct=g) no-repeat center center / cover";
       break;
     case "scattered clouds":
       iconCode = "03d";
+      forecastContainer.style.background =
+        "url(https://media2.giphy.com/media/PIh4laWJlz9bq/giphy.gif?cid=ecf05e471onzfrnepln511m6mm6ly4brsfxn0styxspk17ft&rid=giphy.gif&ct=g) no-repeat center center / cover";
       break;
     case "broken clouds":
       iconCode = "04d";
+      forecastContainer.style.background =
+        "url(https://media4.giphy.com/media/KwZoSJlvep6Vy/giphy.gif?cid=ecf05e471onzfrnepln511m6mm6ly4brsfxn0styxspk17ft&rid=giphy.gif&ct=g) no-repeat center center / cover";
       break;
     case "shower rain":
       iconCode = "09d";
+      forecastContainer.style.background =
+        "url(https://media3.giphy.com/media/t7Qb8655Z1VfBGr5XB/giphy.gif?cid=ecf05e471p9m92z7h5171nt1gqb8yryaxpdd1b2r4e1jaf65&rid=giphy.gif&ct=g) no-repeat center center / cover";
       break;
     case "rain":
       iconCode = "10d";
+      forecastContainer.style.background = "url(https://media3.giphy.com/media/26DMWExfbZSiV0Btm/giphy.gif?cid=ecf05e47q2g6jqmkf5roe2nfq9uz9ti616fyoxcpaqy9ppnt&rid=giphy.gif&ct=g) no-repeat center center / cover"
       break;
     case "thunderstorm":
       iconCode = "11d";
-    forecastContainer.style.backgroundImage = "url(https://media0.giphy.com/media/aPlzgxciAwVj2/giphy.gif?cid=ecf05e471odl2cftb03dc0hfeahz10o5bz9sd1z724x4wsw6&rid=giphy.gif&ct=g)";
+      forecastContainer.style.backgroundImage =
+        "url(https://media0.giphy.com/media/aPlzgxciAwVj2/giphy.gif?cid=ecf05e471odl2cftb03dc0hfeahz10o5bz9sd1z724x4wsw6&rid=giphy.gif&ct=g)";
       break;
     case "snow":
       iconCode = "13d";
+      forecastContainer.style.background = "url(https://media1.giphy.com/media/irxyCZ6H4zS5G/giphy.gif?cid=ecf05e476nzma8h1i5y0eil18x9ox5iq8zowajtof4m7lagw&rid=giphy.gif&ct=g) no-repeat center center / cover"
       break;
     case "mist":
       iconCode = "50d";
+      forecastContainer.style.background = "url(https://media2.giphy.com/media/yhZr5Wx7CBFbq/giphy.gif?cid=ecf05e4791cdropxqd51klfr6r34y4c1nmcx3a2sijuhbbjc&rid=giphy.gif&ct=g) no-repeat center center / cover"
       break;
 
-    default: iconCode = "clear sky"
+    default:
+      iconCode = "clear sky";
       break;
   }
-  weatherImage.setAttribute("src", `http://openweathermap.org/img/wn/${iconCode}@2x.png` )
+  weatherImage.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${iconCode}@2x.png`
+  );
 
   // console.log(data);
 }
-
 
 const weather = async (url) => {
   try {
